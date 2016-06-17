ab47115d7aeda146f7c6c7dd676797910e0e5f95
John Washam <john@readyprompt.com>
2016-06-17 13:30:30 -0700

    Experimenting with forward_list and list.

--- linked_lists/main.cc
+++ linked_lists/main.cc
@@ -1,9 +1,15 @@
 #include <cstdlib>
 #include <iostream>
-#include <string>
 
 #include "src/linked_list.h"
 #include "src/linked_list.cc"
+
+// for stdlib experiments:
+#include <forward_list>
+#include <list>
+
+void stdlib_forward_list_experiment();
+void stdlib_doubly_linked_list_experiment();
 
 int main(int argc, char *argv[]) {
 
@@ -21,5 +27,99 @@ int main(int argc, char *argv[]) {
 
   std::cout << "Hello friends - look in tests directory." << std::endl;
 
+  stdlib_forward_list_experiment();
+  stdlib_doubly_linked_list_experiment();
+
   return EXIT_SUCCESS;
+}
+
+void stdlib_forward_list_experiment() {
+
+  std::cout << "----------------------------" << std::endl;
+  std::cout << "Now messing with standard library's forward list (singly-linked list)" << std::endl;
+
+  std::forward_list<int> fl {100, 200, 300};
+
+  std::cout << "Items:" << std::endl;
+  for (int &item : fl) {
+    std::cout << item << std::endl;
+  }
+
+  std::cout << "First item: " << fl.front() << std::endl;
+
+  std::cout << "Getting items using iterator: ";
+  for (auto it = fl.begin(); it != fl.end(); ++it) {
+    std::cout << *it << " -> ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "Adding to front: " << std::endl;
+  fl.push_front(5);
+  for (auto it = fl.begin(); it != fl.end(); ++it) {
+    std::cout << *it << " -> ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "Popping front 2x: " << std::endl;
+  fl.pop_front();
+  fl.pop_front();
+  for (auto it = fl.begin(); it != fl.end(); ++it) {
+    std::cout << *it << " -> ";
+  }
+  std::cout << std::endl;
+}
+
+void stdlib_doubly_linked_list_experiment() {
+
+  std::cout << "----------------------------" << std::endl;
+  std::cout << "Now messing with standard library's list (doubly-linked list)" << std::endl;
+
+  std::list<int> dl {100, 200, 300};
+
+  std::cout << "Items:" << std::endl;
+  for (int &item : dl) {
+    std::cout << item << std::endl;
+  }
+
+  std::cout << "First item: " << dl.front() << std::endl;
+
+  std::cout << "Getting items using iterator: ";
+  for (auto it = dl.begin(); it != dl.end(); ++it) {
+    std::cout << *it << " -> ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "Adding to front and back: " << std::endl;
+  dl.push_front(5);
+  dl.push_back(400);
+  for (auto it = dl.begin(); it != dl.end(); ++it) {
+    std::cout << *it << " -> ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "Adding to back again: " << std::endl;
+  dl.push_back(500);
+  for (auto it = dl.begin(); it != dl.end(); ++it) {
+    std::cout << *it << " -> ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "Popping front 2x: " << std::endl;
+  dl.pop_front();
+  dl.pop_front();
+  for (auto it = dl.begin(); it != dl.end(); ++it) {
+    std::cout << *it << " -> ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "Popping back 2x: " << std::endl;
+  dl.pop_back();
+  dl.pop_back();
+  for (auto it = dl.begin(); it != dl.end(); ++it) {
+    std::cout << *it << " -> ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "----------------------------" << std::endl;
+  std::cout << "Now messing with standard library's list (doubly-linked list)" << std::endl;
 }
