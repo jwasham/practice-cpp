2bc4e44ef80a88b08430f8531ae225d030e2ce15
John Washam <john@readyprompt.com>
2016-06-19 21:11:56 -0700

    Messing around with std::queue and std::stack.

--- queue_linked_list/main.cc
+++ queue_linked_list/main.cc
@@ -1,19 +1,61 @@
 #include <cstdlib>
 #include <iostream>
 
-#include "src/queue_linked_list.h"
 #include "src/queue_linked_list.cc"
+#include "src/queue_linked_list.h"
 
 // for stdlib experiments:
 #include <queue>
 #include <stack>
 
-//void stdlib_forward_list_experiment();
-//void stdlib_doubly_linked_list_experiment();
+void stdlib_stack_experiment();
+void stdlib_queue_experiment();
 
 int main(int argc, char *argv[]) {
+  std::cout << "====== Stack ========" << std::endl;
+  stdlib_stack_experiment();
 
-  std::cout << "Here we go." << std::endl;
+  std::cout << "====== Queue ========" << std::endl;
+  stdlib_queue_experiment();
 
   return EXIT_SUCCESS;
+}
+
+void stdlib_stack_experiment() {
+  auto my_stack = std::stack<int>();
+
+  std::cout << "Pushed 34" << std::endl;
+  my_stack.push(34);
+  int top = my_stack.top();
+  my_stack.pop();
+  std::cout << "Popped: " << top << std::endl;
+
+  my_stack.push(123);
+  my_stack.push(634);
+  my_stack.push(2372);
+  my_stack.push(134);
+  my_stack.push(7);
+
+  std::cout << "Size: " << my_stack.size() << std::endl;
+
+  while (!my_stack.empty()) {
+    std::cout << "Popping: " << my_stack.top() << std::endl;
+    my_stack.pop();
+  }
+}
+
+void stdlib_queue_experiment() {
+  auto my_q = std::queue<std::string>();
+
+  std::cout << "Pushing " << std::endl;
+  my_q.push("first!");
+  my_q.push("second");
+  my_q.push("third");
+
+  std::cout << "Size: " << my_q.size() << std::endl;
+
+  while (!my_q.empty()) {
+    std::cout << "Popping: " << my_q.front() << std::endl;
+    my_q.pop();
+  }
 }
