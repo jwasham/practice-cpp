d1d445dc051bb63c307933081bc886a4619eb9af
John Washam <john@readyprompt.com>
2016-06-19 19:26:58 -0700

    Setting up project for C++ implementation of a queue using an array.

---
+++ queue_array/main.cc
@@ -0,0 +1,36 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "queue_array.h"
+#include "queue_array.cc"
+
+// for stdlib experiments:
+//#include <forward_list>
+//#include <list>
+
+//void stdlib_forward_list_experiment();
+//void stdlib_doubly_linked_list_experiment();
+
+int main(int argc, char *argv[]) {
+
+//  jw::LinkedList<int> ilist{};
+//  ilist.PushFront(4);
+//  ilist.PushFront(9);
+//
+//  std::cout << "Size of int list is: " << ilist.Size() << std::endl;
+//
+//  jw::LinkedList<std::string> strlist{};
+//  strlist.PushFront("string 1");
+//  strlist.PushFront("another");
+//  strlist.PushFront("and another");
+//  std::cout << "Size of string list is: " << strlist.Size() << std::endl;
+//
+//  std::cout << "Hello friends - look in tests directory." << std::endl;
+//
+//  stdlib_forward_list_experiment();
+//  stdlib_doubly_linked_list_experiment();
+
+  std::cout << "Here we go." << std::endl;
+
+  return EXIT_SUCCESS;
+}
---
+++ queue_array/tests/queue_array_tests.cc
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+#include "queue_array.cc"
+#include "queue_array.h"
+
+int main(int argc, char *argv[]) {
+
+  std::cout << "out" << std::endl;
+
+  return EXIT_SUCCESS;
+}
