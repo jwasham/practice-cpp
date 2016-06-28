7c4996482966b3c1c59e2f06d234827deb478db1
John Washam <john@readyprompt.com>
2016-06-28 14:34:51 -0700

    Added asserts.

--- binary_search_tree/main.cc
+++ binary_search_tree/main.cc
@@ -1,12 +1,13 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <iostream>
-#include "bst_simple.h"
 #include "bst_simple.cc"
+#include "bst_simple.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   using namespace jw;
 
-  BSTNode * root = nullptr;
+  BSTNode* root = nullptr;
 
   root = Insert(root, 15);
   root = Insert(root, 10);
@@ -26,8 +27,11 @@ int main(int argc, char *argv[]) {
   int height = GetHeight(root);
 
   std::cout << "Min: " << min << std::endl;
+  assert(min == 2);
   std::cout << "Max: " << max << std::endl;
+  assert(max == 44);
   std::cout << "Height: " << height << std::endl;
+  assert(height == 5);
 
   std::cout << "Tree items (breadth-first): ";
   PrintBFS(root);
@@ -37,30 +41,42 @@ int main(int argc, char *argv[]) {
   PrintInOrder(root);
   std::cout << std::endl;
 
-  std::cout << "Is a binary search tree: " << IsBinarySearchTree(root) << std::endl;
+  std::cout << "Is a binary search tree: " << IsBinarySearchTree(root)
+            << std::endl;
+  assert(IsBinarySearchTree(root));
 
   BSTNode* suc1 = GetSuccessor(root, 5);
-  std::cout << "Successor to 5 (2 children), should be 7: " << suc1->data << std::endl;
+  std::cout << "Successor to 5 (2 children), should be 7: " << suc1->data
+            << std::endl;
+  assert(suc1->data == 7);
 
   BSTNode* suc1a = GetSuccessor(root, 7);
-  std::cout << "Successor to 7 (0 children), should be 8: " << suc1a->data << std::endl;
+  std::cout << "Successor to 7 (0 children), should be 8: " << suc1a->data
+            << std::endl;
+  assert(suc1a->data == 8);
 
   BSTNode* suc2 = GetSuccessor(root, 12);
-  std::cout << "Successor to 12 (0 children), should be 15: " << suc2->data << std::endl;
+  std::cout << "Successor to 12 (0 children), should be 15: " << suc2->data
+            << std::endl;
+  assert(suc2->data == 15);
 
   BSTNode* suc3 = GetSuccessor(root, 15);
-  std::cout << "Successor to 15 (2 children), should be 16: " << suc3->data << std::endl;
+  std::cout << "Successor to 15 (2 children), should be 16: " << suc3->data
+            << std::endl;
+  assert(suc3->data == 16);
 
   // bad tree
-  BSTNode * otherroot = GetNewNode(25);
-  BSTNode * node1 = GetNewNode(12);
-  BSTNode * node2 = GetNewNode(15);
-  BSTNode * node3 = GetNewNode(85);
+  BSTNode* otherroot = GetNewNode(25);
+  BSTNode* node1 = GetNewNode(12);
+  BSTNode* node2 = GetNewNode(15);
+  BSTNode* node3 = GetNewNode(85);
   otherroot->left = node1;
   otherroot->right = node2;
   otherroot->left->right = node3;
 
-  std::cout << "Is a binary search tree: " << IsBinarySearchTree(otherroot) << std::endl;
+  std::cout << "Is a binary search tree: " << IsBinarySearchTree(otherroot)
+            << std::endl;
+  assert(!IsBinarySearchTree(otherroot));
 
   std::cout << "Removing 2 (no children)" << std::endl;
   root = DeleteValue(root, 2);
